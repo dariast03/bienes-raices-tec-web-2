777800674b1fea17ae1793c0d171284e05b20064
dariast03 <danteariastarifa@gmail.com>
2024-07-27 20:14:59 -0400

    chore: Add PropertyModel class for managing property data

---
+++ model/PropertyModel.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '../../core/Model.php';
+
+class PropertyModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct('propiedad');
+    }
+}
