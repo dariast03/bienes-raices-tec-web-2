b491114c5745e3abb8235944c3744f7414389763
dariast03 <danteariastarifa@gmail.com>
2024-07-28 17:49:07 -0400

    chore: Add admin properties page and navbar layout

---
+++ view/admin/properties/index.php
@@ -0,0 +1,9 @@
+<?php include 'view/admin/layout/header.php' ?>
+
+LISTA DE PROPIEADES
+
+<a class="btn btn-primary" href="<?php echo BASE_URL ?>/admin/propiedades/form">
+    CREAR
+</a>
+
+<?php include 'view/admin/layout/footer.php' ?>
