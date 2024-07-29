66dbf27c516dfc2e1fee0512db62df0c4c7b42d2
dariast03 <danteariastarifa@gmail.com>
2024-07-29 18:24:16 -0400

    feat: list page properties

--- view/admin/properties/index.php
+++ view/admin/properties/index.php
@@ -1,9 +1,47 @@
-<?php include 'view/admin/layout/header.php' ?>
+<?php
+include 'view/admin/layout/header.php';
+require_once 'model/PropertyModel.php';
 
-LISTA DE PROPIEADES
+$propertyModel  = new PropertyModel();
+$propiedades = $propertyModel->get();
+?>
 
-<a class="btn btn-primary" href="<?php echo BASE_URL ?>/admin/propiedades/form">
-    CREAR
-</a>
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h1>LISTA DE PROPIEDADES</h1>
+            <a class="btn btn-primary" href="<?php echo BASE_URL ?>/admin/propiedades/form">
+                CREAR
+            </a>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Dirección</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Superficie </th>
+                        <th scope="col">Nº Hab.</th>
+                        <th scope="col">Estado</th>>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($propiedades as $propiedad) : ?>
+                        <tr>
+                            <th scope="row"><?php echo $propiedad['id'] ?></th>
+                            <td><?php echo $propiedad['direccion'] ?></td>
+                            <td><?php echo number_format($propiedad['precio']) ?></td>
+                            <td><?php echo $propiedad['superficie_total'] ?></td>
+                            <td><?php echo $propiedad['num_habitaciones'] ?></td>
+                            <td><?php echo $propiedad['estado'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 <?php include 'view/admin/layout/footer.php' ?>
